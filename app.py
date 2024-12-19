723440d666534fca25c71ed3b0242b08bef5d3f4
Roberto Zepeda <robertozh29@hotmail.com>
2024-12-19 00:52:32 -0600

    Add form validation

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 import unittest
 import time
 from selenium import webdriver
+from selenium.common import NoSuchElementException
 from selenium.webdriver.common.by import By
 
 
@@ -42,6 +43,13 @@ class TestUsers(unittest.TestCase):
         last_name_input.send_keys(last_name)
         postal_code_input = driver.find_element(By.ID, "postal-code")
         postal_code_input.send_keys(postal_code)
+        continue_btn = driver.find_element(By.XPATH, '//input[@name="continue"]')
+        continue_btn.click()
+        try:
+            error = self.driver.find_element(By.CLASS_NAME, 'error-message-container')
+            return False if error else True
+        except NoSuchElementException:
+            return True
 
     def keep_and_delete(self, keep=2):
         driver = self.driver
@@ -73,9 +81,9 @@ class TestUsers(unittest.TestCase):
         checkout_button.click()
 
         # Filling shipping details
-        self.fill_checkout_information()
-        continue_btn = driver.find_element(By.XPATH, '//input[@name="continue"]')
-        continue_btn.click()
+        information_filled = self.fill_checkout_information()
+        if not information_filled:
+            self.fail("Checkout information form fail. Marking the test as failed.")
 
         # Finish
         finish_btn = driver.find_element(By.XPATH, '//button[@name="finish"]')
@@ -87,7 +95,7 @@ class TestUsers(unittest.TestCase):
 
         assert True
 
-    def _test_problem_user(self):
+    def test_problem_user(self):
         driver = self.driver
         self.username.send_keys("problem_user")
         self.password.send_keys("secret_sauce")
@@ -95,4 +103,25 @@ class TestUsers(unittest.TestCase):
 
         # Adding products to the cart
         items_added = self.add_all_items()
+        cart_button = driver.find_element(By.ID, "shopping_cart_container")
+        cart_button.click()
+
+        # keeping 2 products and deleting the rest
+        self.keep_and_delete()
+        checkout_button = driver.find_element(By.XPATH, '//button[text()="Checkout"]')
+        checkout_button.click()
+
+        # Filling shipping details
+        information_filled = self.fill_checkout_information()
+        if not information_filled:
+                self.fail("Form behaivor unexpected. Marking the test as failed.")
+
+        # Finish
+        finish_btn = driver.find_element(By.XPATH, '//button[@name="finish"]')
+        finish_btn.click()
+
+        # Returning home page
+        back_btn = driver.find_element(By.XPATH, '//button[@name="back-to-products"]')
+        back_btn.click()
+
         assert items_added
