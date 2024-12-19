4ee8e0a4baec011cd9120c19febda87be24905d0
Roberto Zepeda <robertozh29@hotmail.com>
2024-12-19 00:02:49 -0600

    Keep and delete method

--- app.py
+++ app.py
@@ -42,8 +42,19 @@ class TestUsers(unittest.TestCase):
         last_name_input.send_keys(last_name)
         postal_code_input = driver.find_element(By.ID, "postal-code")
         postal_code_input.send_keys(postal_code)
-        continue_btn = driver.find_element(By.XPATH, '//input[@name="continue"]')
-        continue_btn.click()
+
+    def keep_and_delete(self, keep=2):
+        driver = self.driver
+        cart_items = driver.find_elements(By.CLASS_NAME, "cart_item")
+        remove = len(cart_items) - keep if len(cart_items) > keep else 0
+        for i in range(remove):
+            item = cart_items[i]
+            item_button = item.find_element(By.CLASS_NAME, "btn.btn_secondary.btn_small.cart_button")
+            item_button.click()
+
+        cart_items = driver.find_elements(By.CLASS_NAME, "cart_item")
+        succeeded = True if  int(len(cart_items)) == keep else False
+        return succeeded
 
     def test_standard_user(self):
         driver = self.driver
@@ -53,23 +64,18 @@ class TestUsers(unittest.TestCase):
 
         # Adding products to the cart
         self.add_all_items()
-
         cart_button = driver.find_element(By.ID, "shopping_cart_container")
         cart_button.click()
 
-        # Maintaining 2 products and deleting the rest
-        cart_items = driver.find_elements(By.CLASS_NAME, "cart_item")
-        remove = len(cart_items) - 2 if len(cart_items) > 2 else 0
-        for i in range(remove):
-            item = cart_items[i]
-            item_button = item.find_element(By.CLASS_NAME, "btn.btn_secondary.btn_small.cart_button")
-            item_button.click()
-
+        # keeping 2 products and deleting the rest
+        self.keep_and_delete(1)
         checkout_button = driver.find_element(By.XPATH, '//button[text()="Checkout"]')
         checkout_button.click()
 
         # Filling shipping details
         self.fill_checkout_information()
+        continue_btn = driver.find_element(By.XPATH, '//input[@name="continue"]')
+        continue_btn.click()
 
         # Finish
         finish_btn = driver.find_element(By.XPATH, '//button[@name="finish"]')
@@ -79,7 +85,6 @@ class TestUsers(unittest.TestCase):
         back_btn = driver.find_element(By.XPATH, '//button[@name="back-to-products"]')
         back_btn.click()
 
-        time.sleep(5)
         assert True
 
     def _test_problem_user(self):
